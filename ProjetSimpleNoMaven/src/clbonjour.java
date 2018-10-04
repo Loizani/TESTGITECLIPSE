ab49faf9100327c8dd69a4244f96175f1c44db88
Loizani <loizani.slimani@gmail.com>
2018-10-04 15:09:00 +0200

    Test Git Eclipse

---
+++ ProjetSimpleNoMaven/src/clbonjour.java
@@ -0,0 +1,11 @@
+
+public class clbonjour {
+	public  static void main(String[] argv) {
+		try {
+			System.out.println("Bonjour");
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());			
+		}
+	}
+}
